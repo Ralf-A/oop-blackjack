11dcf7f3d08b6c5967645b0272390297b7a3cb36
Ralf-A <ralfanimagi@gmail.com>
2023-03-22 18:13:30 +0200

    class for cards and suits with to string method

--- Player.java
+++ Player.java
@@ -1,12 +1,21 @@
 import java.util.ArrayList;
 
 public class Player {
-    private String nimi;
+    private String name;
     private Hand hand;
+
+    public String getName() {
+        return name;
+    }
+
+    public Float getBalance() {
+        return balance;
+    }
+
     private Float balance;
 
-    public Player(String nimi, ArrayList<String>[] hand, Float balance) {
-        this.nimi = nimi;
+    public Player(String name, ArrayList<String>[] hand, Float balance) {
+        this.name = name;
         this.hand = new Hand();
         this.balance = balance;
     }
